8b995576319b9b2787888878bbf694a931c35bbe
phazelmao <phazeclorox@gmail.com>
2018-12-13 19:34:54 +0000

    coisas desnecessarias levaram uma PATADA

--- server.js
+++ server.js
@@ -1,36 +1,49 @@
 const package = require('./package.json');
 const express = require('express');
-const snekfetch = require('snekfetch');
+const fs = require('fs');
 const app = express();
 const http = require('http').Server(app);
 const port = 8080; // eslint-disable-line
+
+// Load interviews
+let nomes = fs.readdirSync('src/entrevistas');
+let entrevistas = [];
+for(const nome of nomes) {
+    if(nome.split('.')[1] == 'json')
+        entrevistas.push({name: nome.replace('.json', ''), content: require('./src/entrevistas/'+nome)});
+}
+
+function GetEntrevista(entrevista) {
+    for(let e of entrevistas) {
+        if(e.name === entrevista)
+            return e.content;
+    }
+
+    return false;
+}
 
 app.use(express.static(`${__dirname}/src`));
 app.set('views', `${__dirname}/src/views`);
 app.set('view engine', 'ejs');
 
-app.get('/', async(req, res) => {
-    let { body: entrevistas } = await snekfetch.get(`http://localhost:${port}/api/entrevistas`);
-
-    res.render('home', { entrevistas });
+app.get('/', (req, res) => {
+    return res.render('home', { entrevistas });
 });
 
-app.get('/entrevista/:entrevista', async(req, res) => {
-    let { body: data } = await snekfetch.get(`http://localhost:${port}/api/entrevista/${req.params.entrevista}`);
-    let { body: entrevistas } = await snekfetch.get(`http://localhost:${port}/api/entrevistas`);
-    
-
-    res.render('entrevista', { data, entrevistas });
+app.get('/entrevista/:entrevista', (req, res) => {
+    let entrevista = GetEntrevista(req.params.entrevista);
+    if(entrevista === false)
+        return res.status(400).json({message: '400 Bad Request'});
+    else
+        return res.render('entrevista', { data: entrevista, entrevistas });
 });
 
 app.get('/github', (req, res) => {
-    res.redirect(package.homepage)
+    return res.redirect(package.homepage)
 })
 
-app.use('/api', require('./api.js'));
-
 app.use((req, res) => {
-    res.status(404).json({message: '404 Not Found', coolthing: 'opa opa campeao'});
+    return res.status(404).json({message: '404 Not Found', coolthing: 'opa opa campeao'});
 });
 
 http.listen(port, () => {
